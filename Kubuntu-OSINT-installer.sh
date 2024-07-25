bb45e2a68b16eb67d1aa8a2ce8fe6a6451b4533a
1337codes <3@1337.codes>
2024-07-25 08:38:52 +0200

    Update Kubuntu-OSINT-installer.sh

--- Kubuntu-OSINT-installer.sh
+++ Kubuntu-OSINT-installer.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Define the log file location
+LOG_DIR="$HOME/Desktop/logs"
+LOG_FILE="$LOG_DIR/osint_install_error.log"
+
 # Cleanup function to kill the background keep-alive process
 cleanup() {
+    # Kill the background keep-alive process
     kill %1
 }
 
@@ -10,13 +15,9 @@ trap cleanup EXIT
 
 # More frequent keep-alive: every 30 seconds
 while true; do
-  sudo -n true
-  sleep 30
+    sudo -n true
+    sleep 30
 done 2>/dev/null &
-
-# Define the log file location
-LOG_DIR="$HOME/Desktop/logs"
-LOG_FILE="$LOG_DIR/osint_install_error.log"
 
 # Initialize the log file and create the log directory
 init_error_log() {
@@ -49,11 +50,11 @@ setup_path() {
     if ! grep -q 'export PATH=$PATH:$HOME/.local/bin' ~/.bashrc; then
         echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
     fi
-    export PATH=$PATH:$HOME/.local/bin || { echo "Failed to export PATH"; add_to_error_log "Failed to export PATH"; }
+    . ~/.bashrc || { echo "Failed to source .bashrc"; add_to_error_log "Failed to source .bashrc"; }
 }
 
 install_tools() {
-    local tools=(spiderfoot sherlock maltego python3-shodan theharvester webhttrack outguess stegosuite wireshark metagoofil eyewitness exifprobe ruby-bundler recon-ng cherrytree instaloader photon sublist3r osrframework joplin drawing finalrecon cargo pkg-config curl python3-pip pipx python3-exifread python3-fake-useragent yt-dlp keepassxc)
+    local tools=(curl python3-pip pipx)
     for tool in "${tools[@]}"; do
         if ! dpkg -l | grep -qw $tool; then
             sudo apt install $tool -y 2>>"$LOG_FILE" || {
@@ -70,25 +71,14 @@ install_tor_browser() {
     local download_dir="$HOME/Downloads"
     mkdir -p "$download_dir"
 
-    gpg --auto-key-locate nodefault,wkd --locate-keys torbrowser@torproject.org || { echo "Failed to import Tor Browser Developers signing key"; add_to_error_log "Failed to import Tor Browser Developers signing key"; return 1; }
-
-    local keyring_path="$download_dir/tor.keyring"
-    gpg --output "$keyring_path" --export 0xEF6E286DDA85EA2A4BA7DE684E2C6E8793298290 || { echo "Failed to export Tor Browser Developers signing key"; add_to_error_log "Failed to export Tor Browser Developers signing key"; return 1; }
-
     local tor_browser_link="https://www.torproject.org/dist/torbrowser/13.0.14/tor-browser-linux-x86_64-13.0.14.tar.xz"
     local tor_browser_dir="$download_dir/tor-browser"
-
-    if [ -z "$tor_browser_link" ]; then
-        echo "Failed to find Tor Browser download link"
-        add_to_error_log "Failed to find Tor Browser download link"
-        return 1
-    fi
-
     local tor_browser_tarball="$download_dir/$(basename "$tor_browser_link")"
+
     curl -L "$tor_browser_link" -o "$tor_browser_tarball" || { echo "Failed to download Tor Browser"; add_to_error_log "Failed to download Tor Browser"; return 1; }
     curl -L "${tor_browser_link}.asc" -o "${tor_browser_tarball}.asc" || { echo "Failed to download Tor Browser signature"; add_to_error_log "Failed to download Tor Browser signature"; return 1; }
 
-    gpgv --keyring "$keyring_path" "${tor_browser_tarball}.asc" "$tor_browser_tarball" || { echo "Failed to verify Tor Browser signature"; add_to_error_log "Failed to verify Tor Browser signature"; return 1; }
+    gpgv --keyring /usr/share/keyrings/tor-browser.gpg "${tor_browser_tarball}.asc" "$tor_browser_tarball" || { echo "Failed to verify Tor Browser signature"; add_to_error_log "Failed to verify Tor Browser signature"; return 1; }
 
     tar -xf "$tor_browser_tarball" -C "$download_dir" || { echo "Failed to extract Tor Browser"; add_to_error_log "Failed to extract Tor Browser"; return 1; }
 
@@ -107,36 +97,34 @@ install_phoneinfoga() {
 
     if [ ! -f "./phoneinfoga" ]; then
         echo "PhoneInfoga executable not found after installation script."
-        add_to_error_log "Phoneinfoga executable not found after installation script."
+        add_to_error_log "PhoneInfoga executable not found after installation script."
         return 1
     fi
 
-    sudo install ./phoneinfoga /usr/local/bin/phoneinfoga || { echo "Failed to install Phoneinfoga globally"; add_to_error_log "Failed to install Phoneinfoga globally"; return 1; }
+    sudo mv ./phoneinfoga /usr/local/bin/phoneinfoga || { echo "Failed to move PhoneInfoga to /usr/local/bin"; add_to_error_log "Failed to move PhoneInfoga to /usr/local/bin"; return 1; }
 }
 
 install_python_packages() {
     pipx install youtube-dl || { echo "Failed to install youtube-dl"; add_to_error_log "Failed to install youtube-dl"; }
-    pip3 install dnsdumpster || { echo "Failed to install dnsdumpster"; add_to_error_log "Failed to install dnsdumpster"; }
     pipx install h8mail || { echo "Failed to install h8mail"; add_to_error_log "Failed to install h8mail"; }
     pipx install toutatis || { echo "Failed to install toutatis"; add_to_error_log "Failed to install toutatis"; }
-    pip3 install tweepy || { echo "Failed to install tweepy"; add_to_error_log "Failed to install tweepy"; }
-    pip3 install onionsearch || { echo "Failed to install onionsearch"; add_to_error_log "Failed to install onionsearch"; }
+
+    python3 -m pip install --user dnsdumpster || { echo "Failed to install dnsdumpster"; add_to_error_log "Failed to install dnsdumpster"; }
+    python3 -m pip install --user tweepy || { echo "Failed to install tweepy"; add_to_error_log "Failed to install tweepy"; }
+    python3 -m pip install --user onionsearch || { echo "Failed to install onionsearch"; add_to_error_log "Failed to install onionsearch"; }
 }
 
 install_sn0int() {
-    mkdir -p ~/github-tools || { echo "Failed to create github-tools directory"; add_to_error_log "Failed to create github-tools directory"; }
-    cd ~/github-tools || { echo "Failed to navigate to github-tools directory"; add_to_error_log "Failed to navigate to github-tools directory"; }
-    curl -s https://apt.vulns.sexy/kpcyrd.pgp | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/apt-vulns-sexy.gpg || { echo "Failed to add sn0int gpg key"; add_to_error_log "Failed to add sn0int gpg key"; }
-    echo "deb http://apt.vulns.sexy stable main" | sudo tee /etc/apt/sources.list.d/apt-vulns-sexy.list || { echo "Failed to add sn0int to sources list"; add_to_error_log "Failed to add sn0int to sources list"; }
-    sudo apt update || { echo "Failed to update package lists for sn0int"; add_to_error_log "Failed to update package lists for sn0int"; }
-    sudo apt install sn0int -y || { echo "Failed to install sn0int"; add_to_error_log "Failed to install sn0int"; }
+    # Updated installation method due to repository issues
+    echo "Unable to install sn0int from external repository. Please install it manually if needed."
+    add_to_error_log "Unable to install sn0int from external repository. Please install it manually if needed."
 }
 
 update_tj_null_joplin_notebook() {
-    if [ -d "$HOME/Desktop/TJ-OSINT-Notebook" ]; then
-        cd "$HOME/Desktop/TJ-OSINT-Notebook" && git pull || { echo "Failed to update TJ-OSINT-Notebook"; add_to_error_log "Failed to update TJ-OSINT-Notebook"; return 1; }
+    if [ -d "~/Desktop/TJ-OSINT-Notebook" ]; then
+        cd ~/Desktop/TJ-OSINT-Notebook && git pull || { echo "Failed to update TJ-OSINT-Notebook"; add_to_error_log "Failed to update TJ-OSINT-Notebook"; return 1; }
     else
-        cd "$HOME/Desktop" && git clone https://github.com/tjnull/TJ-OSINT-Notebook.git || { echo "Failed to clone TJ-OSINT-Notebook"; add_to_error_log "Failed to clone TJ-OSINT-Notebook"; return 1; }
+        cd ~/Desktop && git clone https://github.com/tjnull/TJ-OSINT-Notebook.git || { echo "Failed to clone TJ-OSINT-Notebook"; add_to_error_log "Failed to clone TJ-OSINT-Notebook"; return 1; }
     fi
 }
 
